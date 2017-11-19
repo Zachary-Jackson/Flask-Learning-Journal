6782f4f8fcd51bd6d4eb0d91137fe8996ddbe175
Zachary-Jackson <jackson.zachary.r@gmail.com>
2017-11-18 21:42:57 -0500

    Basic files for the flask-learning-journal added.
    The basic files for this flask app have been added. I have also put up a requirments.txt file that shows which versions of packages I will be using.

---
+++ flask_journal.py
@@ -0,0 +1,14 @@
+from flask import (Flask)
+
+
+import forms
+import models
+
+DEBUG = True
+PORT = 8000
+HOST = '0.0.0.0'
+
+app = Flask(__name__)
+
+if __name__ == '__main__':
+    app.run(debug=DEBUG, host=HOST, port=PORT)
